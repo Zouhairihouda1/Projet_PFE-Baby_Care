46e37157ee800363cac82cd83b7ed2d72d5af9f5
ZOUHAIRI Houda <houdazouhairi24@gmail.com>
2026-01-16 17:31:39 +0100

    feat : importation des models - ZOUHAIRI Houda
    Made all models available for import in the package.

--- Backend/src/db/models/__init__.py
+++ Backend/src/db/models/__init__.py
@@ -1,1 +1,16 @@
-﻿
+# Rendre tous les modèles disponibles pour l'import
+from .baby_model import Baby
+from .feeding_model import Feeding
+from .diaper_model import Diaper
+from .biometric_model import Biometric
+from .medical_record_model import MedicalRecord
+from .advice_model import Advice
+
+__all__ = [
+    'Baby',
+    'Feeding',
+    'Diaper',
+    'Biometric',
+    'MedicalRecord',
+    'Advice'
+]﻿
